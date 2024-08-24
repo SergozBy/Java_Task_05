f590dad53d0ba36fdf11210f3be0c6da6a11b76a
Sergey Bykhavets <sergozby@gamil.com>
2024-08-25 00:30:49 +0300

    Test -> ParameterizedTest and CsvSource

--- src/test/java/ru/netology/services/VacationServiceTest.java
+++ src/test/java/ru/netology/services/VacationServiceTest.java
@@ -1,38 +1,18 @@
 package ru.netology.services;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class VacationServiceTest {
 
-    @Test
-    void monthToRelaxFirst() {
+    @ParameterizedTest
+    @CsvSource({
+            "10000, 3000, 20000, 3",
+            "100000, 60000, 150000, 2"
+    })
+    void monthToRelax(int income, int expenses, int threshold, int expected) {
         VacationService service = new VacationService();
-
-        // подготавливаем данные:
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-        int expected = 3;
-
-        // вызываем целевой метод:
-        int actual = service.calculate(income, expenses, threshold);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        Assertions.assertEquals(expected, actual);
-
-        System.out.println("You have: " + actual + " month to relax");
-    }
-
-    @Test
-    void monthToRelaxSecond() {
-        VacationService service = new VacationService();
-
-        // подготавливаем данные:
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-        int expected = 2;
 
         // вызываем целевой метод:
         int actual = service.calculate(income, expenses, threshold);
