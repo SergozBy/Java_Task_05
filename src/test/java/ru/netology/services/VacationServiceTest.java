6d475b0795cf7995defdaef5a9d884b3a3c13bed
Sergey Bykhavets <sergozby@gamil.com>
2024-08-25 00:39:49 +0300

    CsvSource -> CSV file

--- src/test/java/ru/netology/services/VacationServiceTest.java
+++ src/test/java/ru/netology/services/VacationServiceTest.java
@@ -2,15 +2,12 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class VacationServiceTest {
 
     @ParameterizedTest
-    @CsvSource({
-            "10000, 3000, 20000, 3",
-            "100000, 60000, 150000, 2"
-    })
+    @CsvFileSource(files = "src/test/resources/vocation.csv")
     void monthToRelax(int income, int expenses, int threshold, int expected) {
         VacationService service = new VacationService();
 
